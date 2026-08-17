39dfe962d973ef53fe6caf04c1fbe92a45a90e8a
A_Mateos <amafe98@hotmail.com>
2026-08-17 22:34:26 +1000

    Update 06_getMDS_matrix_from_dist.sh

--- 03_intraspecific_diversity/06_getMDS_matrix_from_dist.sh
+++ 03_intraspecific_diversity/06_getMDS_matrix_from_dist.sh
@@ -4,11 +4,11 @@ export R_LIBS="/export/home/a-e/amateos/phyluce_test_2/tools/Rlibs"
 
 #This script converts .dist files from ngsDist into MDS matrix files, using the ngsTools script "getMDS.R". 
 
-for infile in $(ls /export/home/a-e/amateos/phyluce_test_2/mapping_tests/results/top100_12_ngs_dist_region/*.dist); do
+for infile in $(ls /your/path/results/12_ngs_dist_region/*.dist); do
  echo "Infile: $infile"
-outfile=$(echo "$infile" | sed 's|top100_12_ngs_dist_region/|top100_12_ngs_dist_region/mds_dist/mds_dist|' | sed 's/\.dist$/.mds/')
+outfile=$(echo "$infile" | sed 's|12_ngs_dist_region/|12_ngs_dist_region/mds_dist/mds_dist|' | sed 's/\.dist$/.mds/')
  n_ind=$(head $infile -n2 | tail -n1)
- tail -n+3 $infile | head -n $n_ind | Rscript --vanilla --slave /export/home/a-e/amateos/phyluce_test_2/tools/ngsTools/Scripts/getMDS.R \
+ tail -n+3 $infile | head -n $n_ind | Rscript --vanilla --slave /your/path/ngsTools/Scripts/getMDS.R \
      --no_header --data_symm -n 4 -m "MDS" -o $outfile
  echo
 done
